bdd1938b12ae37f7f5d44d08aa22fcbd423549ae
Aryan <a94h.73@gmail.com>
2022-08-18 22:09:26 +0430

    Add Github Module for finding users who are not following back

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Modules
+This repository contains python modules written by me that I find funny/useful.
---
+++ main.py
@@ -0,0 +1,5 @@
+from github import Github
+
+if __name__ == "__main__":
+    Github(username="arianhaddadi").get_not_following_back()
+
